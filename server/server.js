acd722ee66cf419ed8b738aaa40713fa3b551b73
pankajkaule <pankajkaule@pankajkaule@gmail.com>
2021-10-13 14:08:36 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -5,9 +5,6 @@ const port = process.env.PORT || 5000;
 var app = express();
 app.use(express.static(publicpath));
 
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>");
-});
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
